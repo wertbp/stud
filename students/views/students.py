a6dc4587cda87f7f97e7a63125e2fce89082cf24
wertbp <piznak.b@gmail.com>
2016-04-07 02:43:39 +0300

    Add sorting for students_list

--- students/views/students.py
+++ students/views/students.py
@@ -1,30 +1,37 @@
-# -*- coding: utf-8 -*-
+	# -*- coding: utf-8 -*-
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from ..models import Student
 
 def students_list(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Володимир',
-         'last_name': u'Рибак',
-         'ticket': 235,
-         'image': 'img/image1.jpg'},
-        {'id': 2,
-         'first_name': u'Гриць',
-         'last_name': u'Борода',
-         'ticket': 2123,
-         'image': 'img/image2.jpg'},
-		{'id': 3,
-         'first_name': u'Павло',
-         'last_name': u'Непийпиво',
-         'ticket': 221,
-         'image': 'img/image3.jpg'},
-    )
+    students = Student.objects.all()
+    
+	# try to order students list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('last_name', 'first_name', 'ticket'):
+        students = students.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            students = students.reverse()
+
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        students = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        students = paginator.page(paginator.num_pages)
+			
     return render(request, 'students/students_list.html',
         {'students': students})
 
+    
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
 
