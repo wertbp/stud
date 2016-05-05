3f2bbcecba89abc11f8201defa519af6491fb56b
wertbp <piznak.b@gmail.com>
2016-05-06 01:41:04 +0300

    Add students_update form and groups_update form

--- students/views/students.py
+++ students/views/students.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.forms import ModelForm
+from django.views.generic import UpdateView, DeleteView
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from crispy_forms.bootstrap import FormActions
 
 from ..models import Student, Group
-from datetime import datetime
 
 
 def students_list(request):
@@ -41,11 +47,11 @@ def students_add(request):
         if request.POST.get('add_button') is not None:
             # errors collection
             errors = {}
-            
+
             # data for student object
             data = {'middle_name': request.POST.get('middle_name'),
                     'notes': request.POST.get('notes')}
-                        
+
             # validate user input
             first_name = request.POST.get('first_name', '').strip()
             if not first_name:
@@ -58,7 +64,7 @@ def students_add(request):
                 errors['last_name'] = u"Прізвище є обов'язковим"
             else:
                 data['last_name'] = last_name
-                
+
             birthday = request.POST.get('birthday', '').strip()
             if not birthday:
                 errors['birthday'] = u"Дата народження є обов'язковою"
@@ -66,18 +72,18 @@ def students_add(request):
                 try:
                     datetime.strptime(birthday, '%Y-%m-%d')
                 except Exception:
-                    errors['birthday'] = u"Введіть коректний формат дати (напр. 1984-12-30)"
+                    errors['birthday'] = \
+                        u"Введіть коректний формат дати (напр. 1984-12-30)"
                 else:
                     data['birthday'] = birthday
-                    
+
             ticket = request.POST.get('ticket', '').strip()
             if not ticket:
                 errors['ticket'] = u"Номер білета є обов'язковим"
             else:
                 data['ticket'] = ticket
-            
+
             student_group = request.POST.get('student_group', '').strip()
-            
             if not student_group:
                 errors['student_group'] = u"Оберіть групу для студента"
             else:
@@ -86,52 +92,26 @@ def students_add(request):
                     errors['student_group'] = u"Оберіть коректну групу"
                 else:
                     data['student_group'] = groups[0]
-                        
+
+
             photo = request.FILES.get('photo')
             if photo:
                 data['photo'] = photo
-                
+
             # save student
             if not errors:
                 student = Student(**data)
                 student.save()
-                
+
                 # redirect to students list
                 return HttpResponseRedirect(
                     u'%s?status_message=Студента успішно додано!' %
                     reverse('home'))
-			
             else:
                 # render form with errors and previous user input
                 return render(request, 'students/students_add.html',
                     {'groups': Group.objects.all().order_by('title'),
-                    'errors': errors})
-                            
-            """if not errors:
-                # create student object
-                student = Student(
-                    first_name=request.POST['first_name'],
-                    last_name=request.POST['last_name'],
-                    middle_name=request.POST['middle_name'],
-                    birthday=request.POST['birthday'],
-                    ticket=request.POST['ticket'],
-                    student_group=
-                        Group.objects.get(pk=request.POST['student_group']),
-                    photo=request.FILES['photo'],
-                )
-
-                # save it to database
-                student.save()
-				
-                # redirect user to students list
-                return HttpResponseRedirect(reverse('home'))
-			
-            else:
-                # render form with errors and previous user input
-                return render(request, 'students/students_add.html',
-                    {'groups': Group.objects.all().order_by('title'),
-                    'errors': errors})"""
-            
+                     'errors': errors})
         elif request.POST.get('cancel_button') is not None:
             # redirect to home page on cancel button
             return HttpResponseRedirect(
@@ -141,10 +121,56 @@ def students_add(request):
         # initial form render
         return render(request, 'students/students_add.html',
             {'groups': Group.objects.all().order_by('title')})
-       
 
-def students_edit(request, sid):
-    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+class StudentUpdateForm(ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
 
-def students_delete(request, sid):
-    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+    def __init__(self, *args, **kwargs):
+        super(StudentUpdateForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+
+        # set form tag attributes
+        self.helper.form_action = reverse('students_edit',
+            kwargs={'pk': kwargs['instance'].id})
+        self.helper.form_method = 'POST'
+        self.helper.form_class = 'form-horizontal'
+
+        # set form field properties
+        self.helper.help_text_inline = True
+        self.helper.html5_required = True
+        self.helper.label_class = 'col-sm-2 control-label'
+        self.helper.field_class = 'col-sm-10'
+
+        # add buttons
+        self.helper.layout[-1] = FormActions(
+            Submit('add_button', u'Зберегти', css_class="btn btn-primary"),
+            Submit('cancel_button', u'Скасувати', css_class="btn btn-link"),
+        )
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    template_name = 'students/students_edit.html'
+    form_class = StudentUpdateForm
+
+    def get_success_url(self):
+        return u'%s?status_message=Студента успішно збережено!' \
+            % reverse('home')
+
+    def post(self, request, *args, **kwargs):
+        if request.POST.get('cancel_button'):
+            return HttpResponseRedirect(
+                u'%s?status_message=Редагування студента відмінено!' %
+                reverse('home'))
+        else:
+            return super(StudentUpdateView, self).post(request, *args, **kwargs)
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'students/students_confirm_delete.html'
+
+    def get_success_url(self):
+        return u'%s?status_message=Студента успішно видалено!' \
+            % reverse('home')
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from students.views.students import StudentUpdateView, StudentDeleteView
+from students.views.groups import GroupAddView, GroupUpdateView, GroupDeleteView
+
 from .settings import MEDIA_ROOT, DEBUG
 
 urlpatterns = patterns('',
@@ -8,27 +11,26 @@ urlpatterns = patterns('',
     url(r'^$', 'students.views.students.students_list', name='home'),
     url(r'^students/add/$', 'students.views.students.students_add',
          name='students_add'),
-    url(r'^students/(?P<sid>\d+)/edit/$',
-         'students.views.students.students_edit',
+    url(r'^students/(?P<pk>\d+)/edit/$',
+         StudentUpdateView.as_view(),
          name='students_edit'),
-    url(r'^students/(?P<sid>\d+)/delete/$',
-         'students.views.students.students_delete',
+    url(r'^students/(?P<pk>\d+)/delete/$',
+         StudentDeleteView.as_view(),
          name='students_delete'),
 
     # Groups urls
     url(r'^groups/$', 'students.views.groups.groups_list', name='groups'),
-    url(r'^groups/add/$', 'students.views.groups.groups_add',
-         name='groups_add'),
-    url(r'^groups/(?P<gid>\d+)/edit/$', 'students.views.groups.groups_edit',
+    url(r'^groups/add/$', GroupAddView.as_view(), name='groups_add'),
+    url(r'^groups/(?P<pk>\d+)/edit/$', GroupUpdateView.as_view(),
          name='groups_edit'),
-    url(r'^groups/(?P<gid>\d+)/delete/$', 'students.views.groups.groups_delete',
+    url(r'^groups/(?P<pk>\d+)/delete/$', GroupDeleteView.as_view(),
          name='groups_delete'),
 
-    url(r'^admin/', include(admin.site.urls)),
-    
     # Contact Admin Form
     url(r'^contact-admin/$', 'students.views.contact_admin.contact_admin',
-        name='contact_admin'),             
+        name='contact_admin'),
+
+    url(r'^admin/', include(admin.site.urls)),
 
 )
 
